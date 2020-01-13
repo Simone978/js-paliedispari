15b76d9de1caa523ddc14d8dce880e1326b62d22
Simone <simonelega.sl@gmail.com>
2020-01-13 15:35:22 +0100

    risolta funzione pari e dispari

--- js/script.js
+++ js/script.js
@@ -1,24 +1,60 @@
 // Scrivi una funzione per capire se la parola è palindroma
 
-var parola = prompt('inserisci una parola');
+// var parola = prompt('inserisci una parola');
+//
+// var risultato = palindroma(parola);
+//
+// if (risultato== true){
+//   console.log(parola + ' è palindroma');
+// }else{
+//   console.log(parola + ' non è palindroma');
+// }
+//
+// function palindroma(word){
+//   var parolaContrario ='';
+//   for (var i = parola.length - 1; i >= 0; i--) {
+//     parolaContrario += parola[i];
+//   }
+//   if(parola == parolaContrario){
+//     parolaContrario = true;
+//   }else{
+//     parolaContrario = false;
+//   }
+//   return parolaContrario;
+// }
 
-var risultato = palindroma(parola);
+// L’utente sceglie pari o dispari e un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1
+// a 5) per il computer.
+// Sommiamo i due numeri e dichiariamo chi ha
+// vinto (controlliamo se la somma da pari o dispari).
 
-if (risultato== true){
-  console.log(parola + ' è palindroma');
+var utentePariDispari = prompt('inserisci pari o dispari');
+var utenteNumero = parseInt(prompt('inserisci un numero'));
+var numeroPc = sceltaPc(1,5);
+var controlloPariDispari = sommaEControlla(utenteNumero, numeroPc);
+
+if(controlloPariDispari == true){
+  console.log('Hai vinto');
 }else{
-  console.log(parola + ' non è palindroma');
+  console.log('Hai perso');
 }
 
-function palindroma(word){
-  var parolaContrario ='';
-  for (var i = parola.length - 1; i >= 0; i--) {
-    parolaContrario += parola[i];
-  }
-  if(parola == parolaContrario){
-    parolaContrario = true;
+// funzione che genera numero random del computer
+function sceltaPc(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+// funzione che somma gli argomenti e controlla se il numero è pari o dispari in relazione alla scelta dell'utente
+function sommaEControlla(numero1, numero2){
+  var risultato='';
+  var somma = numero1 + numero2;
+  if(somma %2 == 0 && utentePariDispari == 'pari'){
+    risultato = true;
+  }else if (somma %2 != 0 && utentePariDispari == 'dispari') {
+    risultato = true;
   }else{
-    parolaContrario = false;
+    risultato = false;
   }
-  return parolaContrario;
+  return risultato;
 }
